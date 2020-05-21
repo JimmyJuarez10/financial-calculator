6a675b3e4671d8b4a9d4920fd3bcd695ecec1ec9
JimmyJuarez <jimjuareza@gmail.com>
2020-05-21 10:52:45 -0700

    Add a handleSubmission() to Interest Calc form

---
+++ src/components/interest-calculator/balance.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const balance = () => {
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>{futureValue}</h1>
+    </>
+  );
+};
--- src/components/interest-calculator/calculateInterest.js
+++ src/components/interest-calculator/calculateInterest.js
@@ -1,60 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export class InterestCalculator extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			period: 0,
-			interest: 0,
-			presentValue: 0,
-		};
-	}
-	handleSubmission() {
-		const base = 1 + this.state.interest;
-		this.state.futureValue =
-			this.state.presentValue * Math.pow(base, this.state.period);
-		console.log(this.state.futureValue);
-	}
-	render() {
-		return (
-			<>
-				<h2>Interest Calculator</h2>
-				<h3> Information Required</h3>
-				<form onSubmit={this.state.handleSubmission}>
-					<div className="form-control">
-						<label htmlFor="presentValue">
-							Enter how much money you have/owe.
-						</label>
-						<input
-							type="number"
-							value={this.state.presentValue}
-							placeholder="Present Value"
-						/>
-					</div>
-					<div className="form-control">
-						<label htmlFor="period">Time (in months) </label>
-						<input type="number" value={this.state.period} placeholder="Time" />
-					</div>
-					<div className="form-control">
-						<label htmlFor="interest">Interest </label>
-						<input
-							type="number"
-							value={this.state.interest}
-							placeholder="Interest"
-						/>
-					</div>
-					<button className="btn">Calculate Interest</button>
-				</form>
-			</>
-		);
-	}
-}
-// export const CalcInterest = () => {
-// 	const [period, setPeriod] = useState();
-// 	const [presentValue, setPresentValue] = useState();
-// 	const [interest, setInterest] = useState();
-// 	const onSubmit = (e) => {
-// 		e.preventDefault();
-// 	};
+export const InterestCalculator = () => {
+  const [period, setPeriod] = useState('');
+  const [interest, setInterest] = useState('');
+  const [presentValue, setPresentValue] = useState('');
 
-// };
+  const handleSubmission = (e) => {
+    const base = 1 + interest;
+    const futureValue = presentValue * Math.pow(base, period);
+    alert(`Your future value is ${futureValue}`);
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <h2>Interest Calculator</h2>
+      <h3> Information Required</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="presentValue">
+            Enter how much money you have/owe.
+          </label>
+          <input
+            type="number"
+            value={presentValue}
+            onChange={(e) => setPresentValue(e.target.value)}
+            placeholder="Present Value"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="period">Time (in months) </label>
+          <input
+            type="number"
+            value={period}
+            onChange={(e) => setPeriod(e.target.value)}
+            placeholder="Time"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="interest">Interest </label>
+          <input
+            type="number"
+            value={interest}
+            onChange={(e) => setInterest(e.target.value)}
+            placeholder="Interest"
+          />
+        </div>
+        <input type="button" value="submit" onClick={handleSubmission} />
+      </form>
+    </>
+  );
+};
